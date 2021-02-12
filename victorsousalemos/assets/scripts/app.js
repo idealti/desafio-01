12b65fbceb79188178041991c9aa7dc5861726ea
Victor de Sousa <victorsousalemos@hotmail.com>
2021-02-12 17:28:37 -0300

    subindo codigo do desafio-01

---
+++ victorsousalemos/assets/scripts/app.js
@@ -0,0 +1,7 @@
+$('#contact-form').on('submit', function() {
+    Swal.fire(
+        'Sucesso!', /* Título da caixinha do Swal2 */
+        'Seu contato foi enviado!', /* Mensagem da caixinha do Swal2 */
+        'success' /* Tipo da caixinha do Swal2 */
+    )
+});
